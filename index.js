504131fb383b39a3a9e46d2ef66d0344a4e0365b
André Schubert <schubert@365farmnet.com>
2020-04-03 11:33:47 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const HTML2PDF = require('./lib/html2pdf.js');
+
+(async () => {
+    await HTML2PDF._initializePlugins();
+    await HTML2PDF.pdf('./templates/test.pug', {"a":"b", "c":"d"});
+})();
