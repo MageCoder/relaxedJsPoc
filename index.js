ccb3dbfc712cd90655749e70e924a4810f51dc7f
André Schubert <schubert@365farmnet.com>
2020-04-03 12:52:40 +0200

    added build-once option

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const HTML2PDF = require('./lib/html2pdf.js');
+const program = require('commander');
+const version = require('./package.json').version
+
+program
+  .version(version)
+  .option('--bo, --build-once', 'Build once only, do not watch');
+
+  // ARGUMENTS PARSING AND SETUP
+console.debug(process.argv);
+program.parse(process.argv);
+console.debug(program);
+
 
 (async () => {
     await HTML2PDF._initializePlugins();
     await HTML2PDF.pdf('./templates/test.pug', {"a":"b", "c":"d"});
 
+    if (program.buildOnce) {
+        process.exit(0)
+    } 
 })();
